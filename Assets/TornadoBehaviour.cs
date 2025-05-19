8e4ef3d035d8c7e5efa862215fff1523ee64942b
Meleznian <jwinmeleznia@gmail.com>
2025-05-19 22:45:55 +1000

    Added Effects to Tornado Ritual

---
+++ Assets/TornadoBehaviour.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TornadoBehaviour : MonoBehaviour
+{
+    public GameObject bottle;
+    public GameObject appearEffect;
+    public GameObject BottledWind;
+    public ParticleSystem fog;
+    public ParticleSystem leaves;
+    public ParticleSystem tornado;
+
+
+    private void Update()
+    {
+        if(fog == null && leaves == null && tornado == null)
+        {
+            Destroy(transform.parent.gameObject);
+        }
+    }
+
+    public void BottleAppear()
+    {
+        Instantiate(appearEffect, bottle.transform.position,bottle.transform.rotation);
+        bottle.GetComponent<MeshRenderer>().enabled = true;
+    }
+
+    public void CreateBottle()
+    {
+        Instantiate(appearEffect, bottle.transform.position, bottle.transform.rotation);
+        Rigidbody rb = Instantiate(BottledWind, bottle.transform.position, bottle.transform.rotation).GetComponent<Rigidbody>();
+        rb.AddExplosionForce(50, transform.position, 3);
+    }
+
+    public void Stop()
+    {
+        tornado.Stop();
+        Destroy(bottle);
+        fog.Stop();
+        leaves.Stop();
+    }
+
+
+}
